3e760e8a53865982cb2b311f10f12e4107f72915
KitchenTable99 <ccbitt23@colby.edu>
2023-03-22 14:13:17 -0700

    write things into database

---
+++ db_writer.py
@@ -0,0 +1,76 @@
+import sqlite3
+import itertools
+import logging
+from typing import List
+from speech_extractor import Speech, PartialCongressDay
+import glob
+
+
+def get_connection(testing: bool = True) -> sqlite3.Connection:
+    if testing:
+        return sqlite3.connect(':memory:')
+    else:
+        return sqlite3.connect('congressional_speeches.db')
+
+
+def create_tables(conn: sqlite3.Connection) -> None:
+    cur = conn.cursor()
+    cur.execute(""" CREATE TABLE IF NOT EXISTS speakers(
+                bioguide TEXT PRIMARY KEY,
+                name     TEXT,
+                UNIQUE(bioguide, name)
+                ) """)
+    cur.execute(""" CREATE TABLE IF NOT EXISTS speeches(
+                id               INTEGER PRIMARY KEY,
+                date             INTEGER,
+                speaker_bioguide TEXT,
+                chamber          INTEGER,
+                content          TEXT,
+                FOREIGN KEY(speaker_bioguide) REFERENCES speakers(bioguide)
+                ) """)
+    
+def insert_into_speaker_table(speeches: List[Speech], conn: sqlite3.Connection) -> None:
+    data = [(speech.speaker_bioguide, speech.speaker_name) for speech in speeches]
+    with conn:
+        conn.executemany("INSERT OR IGNORE INTO speakers VALUES(?, ?)", data)
+
+
+def insert_into_speeches_table(speeches: List[Speech], date: int, chamber: int, conn: sqlite3.Connection) -> None:
+    repeated_date = itertools.repeat(date)
+    repeated_chamber = itertools.repeat(chamber)
+    speaker_bioguide = (speech.speaker_bioguide for speech in speeches)
+    content = (speech.content for speech in speeches)
+
+    data = list(zip(repeated_date, speaker_bioguide, repeated_chamber, content))
+
+    with conn:
+        conn.executemany("INSERT OR IGNORE INTO speeches(date, speaker_bioguide, chamber, content) VALUES(?, ?, ?, ?)", data)
+
+
+def main():
+    logging.basicConfig(level=logging.INFO)
+    conn = get_connection(testing=False)
+    create_tables(conn)
+
+    all_files = glob.glob('./*/**', recursive=True)
+    # for file in all_files[:11]:
+    for file in all_files:
+        if '.json' in file:
+            pdc = PartialCongressDay(file)
+            speeches = pdc.get_speeches()
+            date = pdc.get_date()
+            chamber = pdc.get_chamber()
+
+            insert_into_speaker_table(speeches, conn)
+            insert_into_speeches_table(speeches, date, chamber, conn)
+
+    for row in conn.cursor().execute("SELECT * FROM speeches"):
+        print(row)
+
+    conn.close()
+
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ speech_extractor.py
@@ -0,0 +1,109 @@
+import json
+from datetime import datetime
+import logging
+from typing import List, Dict, Optional
+from dataclasses import dataclass
+
+TEST_JSON = './2017/CREC-2017-11-15/json/CREC-2017-11-15-pt1-PgH9268-3.json'
+
+@dataclass
+class Speech:
+    speaker_bioguide: str
+    speaker_name: str
+    content: str
+
+    @staticmethod
+    def get_special_bioguide(name):
+        if 'The SPEAKER pro tempore' in name:
+            return 'SpeakerProTem'
+        elif 'The ACTING PRESIDENT pro tempore' in name:
+            return 'ActingPresProTem'
+        elif 'The PRESIDING OFFICER' in name:
+            return 'PresidingOfficer'
+        elif 'The SPEAKER' in name:
+            return 'Speaker'
+        else:
+            return None
+
+
+
+class PartialCongressDay:
+
+    def __init__(self, file_path) -> None:
+        logging.info(f'Creating PDC object for {file_path}')
+        self.file_path = file_path
+        with open(file_path, 'r') as fp:
+            self.json_data = json.load(fp)
+
+    def get_date(self) -> int:
+        "Returns the number of seconds since the epoch to the day this object describes"
+        header = self.extract_field('header', self.json_data)
+
+        month = self.extract_field('month', header)
+        year = self.extract_field('year', header)
+        day = self.extract_field('day', header)
+        padded_day = day.rjust(2, '0')
+
+        date = datetime.strptime(f'{month} {padded_day} {year}', '%B %d %Y')
+        logging.info(f'Parsed date as: {date}')
+
+        return int(date.timestamp())
+
+    def get_chamber(self) -> int:
+        """Returns an int representing the chamber where the speeches were given:
+            0 --> House of Representatives
+            1 --> Senate
+        """
+        header: Dict = self.extract_field('header', self.json_data)
+
+        chamber = self.extract_field('chamber', header)
+
+        return 0 if chamber == 'House' else 1
+
+
+    def get_speeches(self) -> List[Speech]:
+        "Returns a list of Speech objects representing each speech for the partial congressional day"
+        json_content = self.extract_field('content', self.json_data)
+
+        to_return = []
+        for speech in json_content:
+            kind = self.extract_field('kind', speech)
+            if kind != 'speech':
+                continue
+
+            speaker_name = self.extract_field('speaker', speech)
+            speaker_bioguide = Speech.get_special_bioguide(speaker_name)
+            if speaker_bioguide is None:
+                speaker_bioguide = self.extract_field('speaker_bioguide', speech, raise_exception=False)
+            # TODO: determine if name always first part of speech
+            content = self.extract_field('text', speech)
+
+            to_return.append(Speech(speaker_bioguide, speaker_name, content))
+
+        return to_return
+
+    def extract_field(self, field, dictionary, raise_exception=True):
+        """Given some dictionary and some field, this will extract the field and raise a ValueError
+        if the field is not present. It will return the contents of the field"""
+        field_content = dictionary.get(field, None)
+        if field_content is None:
+            logging.critical(f'{self.file_path} does not have a {field}')
+
+        if field_content is None and raise_exception:
+            raise ValueError(f'{field} field missing from {self.file_path}')
+
+        return field_content
+
+
+
+def main():
+    logging.basicConfig(level=logging.INFO)
+    pdc = PartialCongressDay(TEST_JSON)
+    # print(json.dumps(pdc.json_data, indent=1))
+    # print(pdc.get_day())
+    # print(pdc.get_chamber())
+    print(pdc.get_speeches())
+
+
+if __name__ == "__main__":
+    main()
