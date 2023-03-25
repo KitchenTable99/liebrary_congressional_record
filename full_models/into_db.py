f998e57f758c188793def4c79b27738e41b8a140
EC2 Default User <ec2-user@ip-172-31-20-159.ec2.internal>
2023-03-25 23:35:05 +0000

    fix memory size

--- full_models/into_db.py
+++ full_models/into_db.py
@@ -1,21 +1,31 @@
 import sqlite3
+import logging
 import itertools
 from top2vec import Top2Vec
 import sys
 
 
 def get_conn(testing=True):
+    logging.debug(f'Creating conn object with {testing = }')
     return sqlite3.connect(':memory:') if testing else sqlite3.connect('topic_models.db')
+
 
 def extract_topics_from_model(model):
     sizes, topic_nums = model.get_topic_sizes()
-    to_return = []
     for size, topic_num in zip(sizes, topic_nums):
         content_array, _, doc_ids = model.search_documents_by_topic(topic_num, size)
-        to_return.extend(zip(doc_ids, content_array, itertools.repeat(topic_num)))
+        yield list(zip(doc_ids.tolist(), content_array.tolist(), itertools.repeat(int(topic_num))))
 
-    return to_return
 
+def write_speeches(labeled_speeches, conn, table):
+    for insert_block in labeled_speeches:
+        logging.debug(f'inserting into {table}')
+        logging.debug(f'{insert_block[0] = }')
+        logging.debug(f'{type(insert_block[0][0]) = }')
+        logging.debug(f'{type(insert_block[0][1]) = }')
+        logging.debug(f'{type(insert_block[0][2]) = }')
+        with conn:
+            conn.executemany(f'INSERT INTO {table}(temp_id, content, topic_num) VALUES(?, ?, ?)', insert_block)
 
 
 def main():
@@ -24,15 +34,14 @@ def main():
     model = Top2Vec.load(f'{model_name}.model')
 
     conn.execute(f"""CREATE TABLE IF NOT EXISTS temp_{model_name}(
-                     temp_id INTEGER PRIMARY KEY
+                     temp_id INTEGER PRIMARY KEY,
                      content TEXT,
                      topic_num INTEGER
                      )""")
     labeled_speeches = extract_topics_from_model(model)
-    print(labeled_speeches)
-
-
+    write_speeches(labeled_speeches, conn, f'temp_{model_name}')
 
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
     main()
