ce56297539075cbe8c57c1a32e6992bf05b0e2bf
EC2 Default User <ec2-user@ip-172-31-20-159.ec2.internal>
2023-03-26 04:10:41 +0000

    add deduplicated universal_sentence model

--- model_into_db.py
+++ model_into_db.py
@@ -13,33 +13,34 @@ def get_conn(testing=True):
 def extract_topics_from_model(model):
     sizes, topic_nums = model.get_topic_sizes()
     for size, topic_num in zip(sizes, topic_nums):
-        content_array, _, doc_ids = model.search_documents_by_topic(topic_num, size)
-        yield list(zip(doc_ids.tolist(), content_array.tolist(), itertools.repeat(int(topic_num))))
+        _, _, doc_ids = model.search_documents_by_topic(topic_num, size)
+        yield list(zip(doc_ids.tolist(), itertools.repeat(int(topic_num))))
 
 
 def write_speeches(labeled_speeches, conn, table):
     for insert_block in labeled_speeches:
-        logging.debug(f'inserting into {table}')
-        logging.debug(f'{insert_block[0] = }')
-        logging.debug(f'{type(insert_block[0][0]) = }')
-        logging.debug(f'{type(insert_block[0][1]) = }')
-        logging.debug(f'{type(insert_block[0][2]) = }')
+        logging.debug(f'{insert_block[0][0] = }')
         with conn:
-            conn.executemany(f'INSERT INTO {table}(temp_id, content, topic_num) VALUES(?, ?, ?)', insert_block)
+            conn.executemany(f'INSERT INTO {table}(speech_id, topic_num) VALUES(?, ?)', insert_block)
 
 
 def main():
     conn = get_conn(testing=False)
     model_name = sys.argv[1]
-    model = Top2Vec.load(f'{model_name}.model')
+    model = Top2Vec.load(f'full_models/{model_name}.model')
 
-    conn.execute(f"""CREATE TABLE IF NOT EXISTS temp_{model_name}(
-                     temp_id INTEGER PRIMARY KEY,
-                     content TEXT,
+    # speech_id is a foreign key, but to a table in a different database, but that isn't supported in sqlite
+    conn.execute(f"""CREATE TABLE IF NOT EXISTS {model_name}(
+                     topic_model_id INTEGER PRIMARY KEY,
+                     speech_id INTEGER,
                      topic_num INTEGER
                      )""")
     labeled_speeches = extract_topics_from_model(model)
-    write_speeches(labeled_speeches, conn, f'temp_{model_name}')
+    write_speeches(labeled_speeches, conn, model_name)
+
+    # for row in conn.execute(f"""SELECT * FROM universal_sentence LIMIT 10"""):
+    #     print(row)
+    conn.close()
 
 
 if __name__ == "__main__":
